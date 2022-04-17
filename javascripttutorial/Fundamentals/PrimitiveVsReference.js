505778130edf605c04a65cc44b38ed4cee69f2b5
raysulkobir <raysulkobir97@gmail.com>
2022-04-17 23:57:57 +0600

    array not complite

--- javascripttutorial/Fundamentals/PrimitiveVsReference.js
+++ javascripttutorial/Fundamentals/PrimitiveVsReference.js
@@ -1,2 +1,16 @@
 // Primitive values
 // Primitive values (null, undefined, boolean, number, string, symbol, and BigInt)
+let name = "Md.Raysul kobir";
+let age = 20;
+console.log(name);
+console.log(age);
+
+let person = {
+    name: "Md.Sajid Rana",
+    age: 30,
+}
+console.log(person);
+
+
+person.ss = "ss";
+console.log(person);
---
+++ javascripttutorial/Fundamentals/array.js
@@ -0,0 +1,17 @@
+let arr1 = new Array();
+console.log(arr1);
+
+let arr2 = new Array(1, 2, 3, 4, 5);
+console.log(arr2);
+
+let arr3 = Array(1, 2, 3, 4, 5);
+console.log(arr3);
+
+let arr4 = ["a", "b", "c"];
+console.log(arr4);
+console.log(arr4.length);
+console.log(arr4[0]);
+
+let arr5 = [1, "b"];
+console.log(arr5);
+console.log(typeof(arr5));

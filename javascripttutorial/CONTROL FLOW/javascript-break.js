d48f1c1d39d04861e6203a16b6edce2e144bb146
raysulkobir <raysulkobir97@gmail.com>
2022-05-14 23:58:26 +0600

    javascript-break done

---
+++ javascripttutorial/CONTROL FLOW/javascript-break.js
@@ -0,0 +1,67 @@
+// for(i=1; i< 10; i++){
+//     console.log(i);
+// }
+
+
+// for(i=1; i< 10; i++){
+//     console.log(i);
+//     if(i == 2){
+//         break;
+//     }
+// }
+
+// for(let i = 1; i <= 3; i++){
+//     for(let j = 0; j <= i; j++){
+//         console.log(i, j);
+//     }
+// }
+
+
+// for (let i = 1; i <= 3; i++) {
+//   for (let j = 1; j <= 3; j++) {
+//     if (i + j == 4) {
+//       break;
+//     }
+//     console.log(i, j);
+//   }
+// }
+
+
+// outer: for (let i = 1; i <= 3; i++) {
+//   for (let j = 1; j <= 3; j++) {
+//     if (i + j == 4) {
+//       break outer;
+//     }
+//     console.log(i, j);
+//   }
+// }
+
+
+
+// let i = 0;
+
+// while (i <= 5) {
+//   i++;
+//   console.log(i);
+//   if(i == 3){
+//       break;
+//   }
+// }
+
+
+// let i = 0;
+
+// do {
+//   i++;
+//   console.log(i);
+// } while (i < 5);
+
+
+
+do {
+  i++;
+  console.log(i);
+  if (i == 3) {
+    break;
+  }
+} while (i < 5);
